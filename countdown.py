bd1b9cd186c801b5bc4fb13d0951ec8d7ea2b29c
Prateek Sahay <ptkinvent@gmail.com>
2023-05-06 14:04:55 -0700

    Fixed bug causing countdown script to crash

--- countdown.py
+++ countdown.py
@@ -9,51 +9,50 @@ from PIL import Image, ImageDraw, ImageFont
 from lib.EinkScreen import EinkScreen
 
 
+def draw_centered_text(drawer, y, text, font):
+    screen_width, screen_height = drawer.im.size
+    w, h = drawer.textsize(text, font)
+    drawer.text(xy=(screen_width/2 - w/2, y), text=text, font=font, fill=0)
+
 
 def main():
     with open(sys.argv[1]) as config:
         config = json.load(config)
 
     countdown_date = datetime.datetime.strptime(config['countdown_date'], '%m/%d/%Y').date()
-    heading = config['heading']
-    subheading = config['subheading']
+    heading_text = config['heading']
+    subheading_text = config['subheading']
+    fontsize = config['fontsize']
     prev_delta = 0
-    font72 = ImageFont.truetype('fonts/SourceSansPro.ttf', 72)
-    font144 = ImageFont.truetype('fonts/SourceSansPro.ttf', 144)
+    font_heading = ImageFont.truetype('fonts/SourceSansPro.ttf', fontsize)
+    font_countdown = ImageFont.truetype('fonts/SourceSansPro.ttf', 144)
+    screen = EinkScreen()
 
     while True:
-        screen = EinkScreen()
-
-        def draw_centered_text(drawer, y, text, font):
-            screen_width, screen_height = drawer.im.size
-            w, h = drawer.textsize(text, font)
-            drawer.text(xy=(screen_width/2 - w/2, y), text=text, font=font, fill=0)
-
         delta = (countdown_date - datetime.date.today()).days
         if prev_delta != delta:
             print(f'Updating, {delta} days remaining')
 
-            black_image = Image.new(mode='1', size=(screen.width, screen.height), color=255)  # 255: clear the frame
-            red_image = Image.new(mode='1', size=(screen.width, screen.height), color=255)  # 255: clear the frame
+            black_image = Image.new(mode='1', size=(screen.width, screen.height), color=255)
+            red_image = Image.new(mode='1', size=(screen.width, screen.height), color=255)
 
             black_image_drawer = ImageDraw.Draw(black_image)
             red_image_drawer = ImageDraw.Draw(red_image)
 
             if delta > 0:
-                draw_centered_text(black_image_drawer, 50, heading, font72)
-                draw_centered_text(red_image_drawer, 150, f"{delta}", font144)
-                draw_centered_text(black_image_drawer, 350, subheading, font72)
+                draw_centered_text(black_image_drawer, 50, heading_text, font_heading)
+                draw_centered_text(red_image_drawer, 150, f"{delta}", font_countdown)
+                draw_centered_text(black_image_drawer, 350, subheading_text, font_heading)
             elif delta == 0:
-                draw_centered_text(black_image_drawer, 50, heading, font72)
-                draw_centered_text(red_image_drawer, 150, f"{delta}", font144)
-                draw_centered_text(black_image_drawer, 350, subheading, font72)
+                draw_centered_text(black_image_drawer, 50, heading_text, font_heading)
+                draw_centered_text(red_image_drawer, 150, f"{delta}", font_countdown)
+                draw_centered_text(black_image_drawer, 350, subheading_text, font_heading)
             else:
-                draw_centered_text(black_image_drawer, 50, heading, font72)
-                draw_centered_text(red_image_drawer, 150, f"{-delta}", font144)
-                draw_centered_text(black_image_drawer, 350, subheading, font72)
+                draw_centered_text(black_image_drawer, 50, heading_text, font_heading)
+                draw_centered_text(red_image_drawer, 150, f"{-delta}", font_countdown)
+                draw_centered_text(black_image_drawer, 350, subheading_text, font_heading)
 
             screen.draw(black_image, red_image)
-            del(screen)
             prev_delta = delta
         else:
             print('Not updating')
--- draw-black-bmp.py
+++ draw-black-bmp.py
@@ -8,14 +8,12 @@ from lib.EinkScreen import EinkScreen
 
 
 def main():
-    screen = EinkScreen()
-
-    black_image = Image.new(mode='1', size=(screen.width, screen.height), color=255)  # 255: clear the frame
-    red_image = Image.new(mode='1', size=(screen.width, screen.height), color=255)  # 255: clear the frame
-
     bmp = Image.open(sys.argv[1])
-    black_image.paste(bmp, (0, 0))
 
+    screen = EinkScreen()
+    black_image = Image.new(mode='1', size=(screen.width, screen.height), color=255)
+    red_image = Image.new(mode='1', size=(screen.width, screen.height), color=255)
+    black_image.paste(bmp, (0, 0))
     screen.draw(black_image, red_image)
 
 
